994442bf9ec99151c03db4677dbc00c10b9b55c4
Alex <aleck012@gmail.com>
2024-06-11 19:54:31 +0300

    fixed err with rom num

--- Calculator.go
+++ Calculator.go
@@ -52,30 +52,57 @@ func romanNumCheck (num string) int {
 }
 
 func romanNumGet (num int) string {
-	switch num {
-		case 1:
-			return "I"
-		case 2:
-			return "II"
-		case 3:
-			return "III"
-		case 4:
-			return "IV"
-		case 5:
-			return "V"
-		case 6:
-			return "VI"
-		case 7:
-			return "VII"
-		case 8:
-			return "VIII"
-		case 9:
-			return "IX"
-		case 10:
-			return "X"
-		default:
-			return "0"
+	// divide the number into tens and ones
+	var
+		dec int = (num / 10) * 10
+	var
+		ones int = num % 10
+
+	result := func(num int) string {
+		switch num {
+			case 1:
+				return "I"
+			case 2:
+				return "II"
+			case 3:
+				return "III"
+			case 4:
+				return "IV"
+			case 5:
+				return "V"
+			case 6:
+				return "VI"
+			case 7:
+				return "VII"
+			case 8:
+				return "VIII"
+			case 9:
+				return "IX"
+			case 10:
+				return "X"
+			case 20:
+				return "XX"
+			case 30:
+				return "XXX"
+			case 40:
+				return "XL"
+			case 50:
+				return "L"
+			case 60:
+				return "LX"
+			case 70:
+				return "LXX"
+			case 80:
+				return "LXXX"
+			case 90:
+				return "XC"
+			case 100:
+				return "C"
+			default:
+				return ""
+		}
 	}
+	return result(dec) + result(ones)
 }
 
 func main() {
@@ -118,6 +145,5 @@ func main() {
 			panic("Invalid arguments!")
 		}
 		fmt.Println("Result: ", calculate(num1, num2, operation))
-	}
-	
+	}	
 }
